1d8e16e32fe62e35697b94f61b8832c8f399aef2
AngeliHerimpitia <a43027485@gmail.com>
2026-01-06 14:13:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-let display = document.getElementById('result');
+const display = document.getElementById('result');
+
 function appendToDisplay(value) {
     display.value += value;
 }
-function clearDisplay(){
+
+function clearDisplay() {
     display.value = '';
 }
+
 function deleteLast() {
     display.value = display.value.slice(0, -1);
 }
+
 function calculate() {
     try {
-        // Repmlace × par * pour l'evaluation 
-        let expression = display.value.replace('×','*');
-        display.value = eval(expression);
-    } catch (errror) {
+        // Convertir × et ÷ en opérateurs JS
+        let expression = display.value.replace(/×/g, '*').replace(/÷/g, '/');
+        // Évaluer
+        let result = eval(expression);
+        display.value = result;
+    } catch (error) {
         display.value = 'Erreur';
     }
 }
